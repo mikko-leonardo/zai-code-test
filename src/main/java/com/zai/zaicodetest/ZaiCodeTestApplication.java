3a94cfc0a65dde44b0e660c29e7176b643163c2a
Mikko Leonardo <mikkoleonardo@gmail.com>
2025-05-12 16:41:32 +0800

    Generated Project from Spring Initializr Initial Commit

---
+++ src/main/java/com/zai/zaicodetest/ZaiCodeTestApplication.java
@@ -0,0 +1,13 @@
+package com.zai.zaicodetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaiCodeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaiCodeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zai/zaicodetest/ZaiCodeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zai.zaicodetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaiCodeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
